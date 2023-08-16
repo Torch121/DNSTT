e24d4ca86deef7e1f7cf38a928b6196557ffebff
Torch121 <55146714+Torch121@users.noreply.github.com>
2023-08-16 09:32:53 +0100

    installer.sh

--- installer.sh
+++ installer.sh
@@ -6,7 +6,7 @@ sudo apt install git nano golang-go -y
 
 # Clone the repository and build dnstt-server
 cd /root
-git clone https://www.bamsoftware.com/git/dnstt.git
+git clone http://www.bamsoftware.com/git/dnstt.git
 cd root/dnstt/dnstt-server
 go build
 
